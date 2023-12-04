d388fbc69593c34a1a5a93f232b383d2c4b6c8a0
Rajan-Rizki <148296691+Rajan-Rizki@users.noreply.github.com>
2023-12-04 23:23:50 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let operator = confirm("Ingin menghitung luas/Area bangunan berbentuk persegi pa
 let num1 = parseFloat(prompt("Berapa Tingginya/Height dari bangunan itu? (Meter, jika cm, maka 0, misal 0.25"));
 let num2 = parseFloat(prompt("Berapakah Lebarnya/Width dari bangunan itu? (Meter, jika cm, maka 0, misal 0.25"));
 
-if (operator === true) {
+if (operator) {
   document.write(`<h1> Luas/Area: ${num1 * num2} m²</h1>`);
-}
+} else {
+  document.write('<h1>INVALID</h1>') 
+} 
+
